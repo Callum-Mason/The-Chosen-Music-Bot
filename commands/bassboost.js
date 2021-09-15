8b024e16163c402958330f130c1b56528be75df2
Callum Mason <callum@masons.ml>
2021-09-16 00:28:06 +0100

    updated volumes

--- commands/bassboost.js
+++ commands/bassboost.js
@@ -1,10 +1,14 @@
-const { MessageEmbed } = require("discord.js");
-const { TrackUtils } = require("erela.js");
+const {
+    MessageEmbed
+} = require("discord.js");
+const {
+    TrackUtils
+} = require("erela.js");
 const levels = {
     none: 0.0,
-    low: 0.2,
-    medium: 0.3,
-    high: 0.35,
+    low: 0.25,
+    medium: 0.50,
+    high: 1.0,
 };
 module.exports = {
     name: "bassboost",
@@ -22,7 +26,9 @@ module.exports = {
      * @param {string[]} args
      * @param {*} param3
      */
-    run: async (client, message, args, { GuildDB }) => {
+    run: async (client, message, args, {
+        GuildDB
+    }) => {
 
         let player = await client.Manager.get(message.guild.id);
         if (!player) return client.sendTime(message.channel, "❌ | **Nothing is playing right now...**");
@@ -34,20 +40,21 @@ module.exports = {
         let level = "none";
         if (args.length && args[0].toLowerCase() in levels) level = args[0].toLowerCase();
 
-        player.setEQ(...new Array(3).fill(null).map((_, i) => ({ band: i, gain: levels[level] })));
+        player.setEQ(...new Array(3).fill(null).map((_, i) => ({
+            band: i,
+            gain: levels[level]
+        })));
 
         return client.sendTime(message.channel, `✅ | **Bassboost level set to** \`${level}\``);
     },
     SlashCommand: {
-        options: [
-            {
-                name: "level",
-                description: `Please provide a bassboost level. Available Levels: low, medium, high, or none`,
-                value: "[level]",
-                type: 3,
-                required: true,
-            },
-        ],
+        options: [{
+            name: "level",
+            description: `Please provide a bassboost level. Available Levels: low, medium, high, or none`,
+            value: "[level]",
+            type: 3,
+            required: true,
+        }, ],
         /**
          *
          * @param {import("../structures/DiscordMusicBot")} client
@@ -56,12 +63,14 @@ module.exports = {
          * @param {*} param3
          */
 
-        run: async (client, interaction, args, { GuildDB }) => {
+        run: async (client, interaction, args, {
+            GuildDB
+        }) => {
             const levels = {
                 none: 0.0,
-                low: 0.2,
-                medium: 0.3,
-                high: 0.35,
+                low: 0.25,
+                medium: 0.50,
+                high: 1.0,
             };
 
             let player = await client.Manager.get(interaction.guild_id);
@@ -76,7 +85,10 @@ module.exports = {
             let level = "none";
             if (args.length && args[0].value in levels) level = args[0].value;
 
-            player.setEQ(...new Array(3).fill(null).map((_, i) => ({ band: i, gain: levels[level] })));
+            player.setEQ(...new Array(3).fill(null).map((_, i) => ({
+                band: i,
+                gain: levels[level]
+            })));
 
             return client.sendTime(interaction, `✅ | **Set the bassboost level to** \`${level}\``);
         },
